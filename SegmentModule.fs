9e777439d616f485095a9b0c258dfed8a58c0aab
nishisandali <nishi.sandali@gmail.com>
2018-04-24 12:50:14 +1000

    FSharp Pure Version

---
+++ SegmentModule.fs
@@ -0,0 +1,90 @@
+﻿module SegmentModule
+
+type Coordinate = (int * int) // x, y coordinate of a pixel
+type Colour = byte list       // one entry for each colour band, typically: [red, green and blue]
+
+type Segment = 
+    | Pixel of Coordinate * Colour
+    | Parent of Segment * Segment 
+
+let mapreduce m r = (List.map m) >> (List.reduce r)
+
+let sqr x : float  = x * x
+
+let inline add x y = x + y
+
+let inline mul (x:float) (y:float) : float = y * x
+
+let inline sub x y = x - y
+
+let sum  = List.reduce (+)
+
+let rec createColourList segment  =
+    match segment with
+    |Pixel(_, col) -> [yield col]
+    |Parent(seg1,seg2) -> [yield! (createColourList seg1); yield! (createColourList seg2)]
+
+let createRedColourBand (segment: Segment) (i : int) : float list =
+    let col = segment |> createColourList 
+    [yield float(col.Item(i).Item(0))]
+
+let createGreenColourBand (segment: Segment) (j : int) : float list =
+    let col = segment |> createColourList 
+    [yield float(col.Item(j).Item(1))]
+   
+let createBlueColourBand (segment: Segment) (k : int) : float list=
+    let col = segment |> createColourList 
+    [yield float(col.Item(k).Item(2))]
+
+// - function which calculates standard deviation of a list of numbers
+let standardDeviation (colourBand : float list) : float = 
+    let length = mapreduce (fun x -> 1) (+)
+    let mean = (sum colourBand) / float(length colourBand)
+    let variance = colourBand |> List.averageBy (fun x -> (sqr(x - mean)))
+    sqrt(variance)
+
+// return a list of the standard deviations of the pixel colours in the given segment
+// the list contains one entry for each colour band, typically: [red, green and blue]
+let stddev (segment: Segment) : float list =
+    //raise (System.NotImplementedException())
+
+    let stdevR = createRedColourBand segment 0 |> standardDeviation
+    let stdevG = createGreenColourBand segment 1|> standardDeviation
+    let stdevB = createBlueColourBand segment 2|> standardDeviation
+
+    [stdevR;stdevG;stdevB]
+
+// determine the cost of merging the given segments: 
+// equal to the standard deviation of the combined the segments minus the sum of the standard deviations of the individual segments, 
+// weighted by their respective sizes and summed over all colour bands
+let mergeCost segment1 segment2 : float = 
+    //raise (System.NotImplementedException())
+    let pixel = segment1 |> createColourList
+    let pixel_length = pixel.Length
+    let pixel_lengthFloat = float(pixel_length)
+    let col1 = stddev segment1 
+    let col1Red = col1.Item(0)
+    let col1Green = col1.Item(1)
+    let col1Blue = col1.Item(2)
+
+    let pixel2 = segment2 |> createColourList
+    let pixel_length2 = pixel2.Length
+    let pixel_lengthFloat2 = float(pixel_length2)
+    let col2 = stddev segment2 
+    let col2Red = col2.Item(0)
+    let col2Green = col2.Item(1)
+    let col2Blue = col2.Item(2)
+
+    let pixlencom = add pixel_lengthFloat pixel_lengthFloat2
+    let comcol = stddev (Parent(segment1,segment2))
+    let comcolRed = comcol.Item(0)
+    let comcolGreen = comcol.Item(1)
+    let comcolBlue = comcol.Item(2)
+
+    let merge_costR = (sub (mul comcolRed pixlencom) (add (mul col1Red pixel_lengthFloat) (mul col2Red pixel_lengthFloat2)))
+    let merge_costG = (sub (mul comcolGreen pixlencom) (add (mul col1Green pixel_lengthFloat) (mul col2Green pixel_lengthFloat2)))
+    let merge_costB = (sub (mul comcolBlue pixlencom) (add (mul col1Blue pixel_lengthFloat) (mul col2Blue pixel_lengthFloat2)))
+
+    let merge_costh = add merge_costR merge_costG
+
+    add merge_costh merge_costB
---
+++ SegmentationModule.fs
@@ -0,0 +1,144 @@
+﻿module SegmentationModule
+
+open SegmentModule
+
+// Maps segments to their immediate parent segment that they are contained within (if any) 
+type Segmentation = Map<Segment, Segment>
+
+// Find the largest/top level segment that the given segment is a part of (based on the current segmentation)
+let rec findRoot (segmentation: Segmentation) segment : Segment =
+    let map1 = segmentation
+    let result = Map.tryFind segment map1
+    match result with
+    |Some segment -> (findRoot segmentation segment)
+    |None -> segment
+
+// Initially, every pixel/coordinate in the image is a separate Segment
+// Note: this is a higher order function which given an image, 
+// returns a function which maps each coordinate to its corresponding (initial) Segment (of kind Pixel)
+let createPixelMap (image:TiffModule.Image) : (Coordinate -> Segment) =
+    let pixelMap (coord:Coordinate) =
+        let colourlist = TiffModule.getColourBands image coord
+        Pixel(coord, colourlist)
+    pixelMap
+
+
+// Find the neighbouring segments of the given segment (assuming we are only segmenting the top corner of the image of size 2^N x 2^N)
+// Note: this is a higher order function which given a pixelMap function and a size N, 
+// returns a function which given a current segmentation, returns the set of Segments which are neighbours of a given segment
+let createNeighboursFunction (pixelMap:Coordinate->Segment) (N:int) : (Segmentation -> Segment -> Set<Segment>) =
+    let rec neighbourPixels (segment: Segment) : seq<Coordinate> = 
+        let width = 1 <<< N
+        let height = 1 <<< N
+        match segment with
+        | Pixel ((x,y), _) ->
+                seq { 
+                    if (x+1 < width) then
+                        yield (x+1,y)
+                    if (y+1 < height) then
+                        yield (x,y+1) 
+                    if (x > 0) then
+                         yield (x-1,y)
+                    if (y > 0) then
+                         yield (x,y-1) }
+        | Parent (seg1, seg2) -> seq{yield! (neighbourPixels seg1); yield! (neighbourPixels seg2)}
+
+    let neighbourPix (segment: Segment) : seq<Segment> =
+        let coordinates = neighbourPixels segment
+        coordinates |> Seq.map (fun x -> pixelMap x)
+
+    let neighbhours (segmentation:Segmentation) (segment: Segment) : Set<Segment> =
+        //raise (System.NotImplementedException())
+        let segmentseq = neighbourPix segment
+        let neighbourpixels = Seq.map (findRoot segmentation) segmentseq |> Set.ofSeq |> Set.remove segment
+        neighbourpixels
+    neighbhours
+
+// The following are also higher order functions, which given some inputs, return a function which ...
+
+
+ // Find the neighbour(s) of the given segment that has the (equal) best merge cost
+ // (exclude neighbours if their merge cost is greater than the threshold)
+let createBestNeighbourFunction (neighbours:Segmentation->Segment->Set<Segment>) (threshold:float) : (Segmentation->Segment->Set<Segment>) =
+    let bestNeighbours (segmentation: Segmentation) (segment:  Segment) : Set<Segment> = 
+        let neighboursAll = neighbours segmentation segment 
+        let setGuard = 
+            if (neighboursAll.IsEmpty) then 
+                Set.empty
+            else
+                neighboursAll
+        let mergecosts = Set.map (mergeCost segment) setGuard
+        let bestCostNeightbour seg = 
+            let cost = mergeCost segment seg
+            let bestMergeCost = mergecosts.MinimumElement
+            cost <= threshold && cost <= bestMergeCost && bestMergeCost <= threshold
+        Set.filter bestCostNeightbour neighboursAll
+    bestNeighbours
+
+
+// Try to find a neighbouring segmentB such that:
+//     1) segmentB is one of the best neighbours of segment A, and 
+//     2) segmentA is one of the best neighbours of segment B
+// if such a mutally optimal neighbour exists then merge them,
+// otherwise, choose one of segmentA's best neighbours (if any) and try to grow it instead (gradient descent)
+let createTryGrowOneSegmentFunction (bestNeighbours:Segmentation->Segment->Set<Segment>) (pixelMap:Coordinate->Segment) : (Segmentation->Coordinate->Segmentation) =
+    let rec tryGrowOneSegmentHelp (segmentation : Segmentation) (segment: Segment) : Segmentation =
+        let bestNeighboursofSegment = bestNeighbours segmentation segment
+        let isEachotherBestNeighbours segment2 =
+           let bestNeighbourofSegment2 = bestNeighbours segmentation segment2
+           Set.contains segment bestNeighbourofSegment2
+        let bestNeighbourSegments = Set.filter isEachotherBestNeighbours bestNeighboursofSegment
+        if (bestNeighbourSegments.IsEmpty) then
+            if (bestNeighboursofSegment.IsEmpty) then
+                segmentation
+            else
+                let bestNeoghboursofSegmentAlist = Set.toList bestNeighboursofSegment
+                let segment2 = List.head bestNeoghboursofSegmentAlist
+                tryGrowOneSegmentHelp segmentation segment2
+        else 
+            let bestNeighboursSegmentslist = Set.toList bestNeighbourSegments
+            let segment2 = List.head bestNeighboursSegmentslist
+            let ParentNew = Parent(segment, segment2)
+            segmentation
+            |> Map.add segment ParentNew
+            |> Map.add segment2 ParentNew
+
+    let tryGrowOneSegment (segmentation: Segmentation) (coord: Coordinate) : Segmentation =
+        let baseSegment = pixelMap coord
+        let rootSegement = findRoot segmentation baseSegment
+        tryGrowOneSegmentHelp segmentation rootSegement
+    tryGrowOneSegment
+
+// Try to grow the segments corresponding to every pixel on the image in turn 
+// (considering pixel coordinates in special dither order)
+let createTryGrowAllCoordinatesFunction (tryGrowPixel:Segmentation->Coordinate->Segmentation) (N:int) : (Segmentation->Segmentation) =
+    let tryGrowAllCoordinates(segmentation : Segmentation) : Segmentation =
+        Seq.fold tryGrowPixel segmentation (DitherModule.coordinates N)
+    tryGrowAllCoordinates 
+
+
+// Keep growing segments as above until no further merging is possible
+let createGrowUntilNoChangeFunction (tryGrowAllCoordinates:Segmentation->Segmentation) : (Segmentation->Segmentation) =
+    let rec growUntilNoChange (segmentation: Segmentation) : Segmentation =
+        let segmentationGrowAll = tryGrowAllCoordinates segmentation
+        let segentationGrowForAll = tryGrowAllCoordinates segmentationGrowAll
+        if (segmentationGrowAll = segmentation) then
+            segmentationGrowAll
+        else
+            segentationGrowForAll
+    growUntilNoChange
+
+// Segment the given image based on the given merge cost threshold, but only for the top left corner of the image of size (2^N x 2^N)
+let segment (image:TiffModule.Image) (N: int) (threshold:float)  : (Coordinate -> Segment) =
+    let startSegmentation = Map.empty
+    let segmentPixelMap = createPixelMap image
+    let allNeighbours = createNeighboursFunction segmentPixelMap N
+    let bestOfAllNeighbours = createBestNeighbourFunction allNeighbours threshold
+    let growOneSegmentforBest = createTryGrowOneSegmentFunction bestOfAllNeighbours segmentPixelMap
+    let growAllCoordinatesforBest = createTryGrowAllCoordinatesFunction growOneSegmentforBest N
+    let segmentationLast = growAllCoordinatesforBest startSegmentation
+
+    let findSegmentation (coord : Coordinate) : Segment=
+        let lastPixelMap = segmentPixelMap coord
+        findRoot segmentationLast lastPixelMap
+    findSegmentation
